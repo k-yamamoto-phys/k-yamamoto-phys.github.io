0fc09cea1591c13cfec219552b6c2ad7c2d4637d
Juntaro Fujii <capri4wrk@gmail.com>
2026-07-15 10:03:11 +0000

    Update sitemap configuration to include group paths and adjust existing URLs

--- personal_website/next-sitemap.config.js
+++ personal_website/next-sitemap.config.js
@@ -1,12 +1,36 @@
 /** @type {import('next-sitemap').IConfig} */
 import { siteMetadata } from '../site_data/personal/_metadata.js'
+
+const groupPaths = [
+    '',
+    '/activities',
+    '/contact',
+    '/members',
+    '/presentations',
+    '/project',
+    '/publications',
+    '/research',
+    '/ja',
+    '/ja/activities',
+    '/ja/contact',
+    '/ja/members',
+    '/ja/presentations',
+    '/ja/project',
+    '/ja/publications',
+    '/ja/research',
+];
+
 const config = {
     siteUrl: siteMetadata.publicURL,
     generateRobotsTxt: true, // (optional)
     // ...other options
     autoLastmod: false,
     generateIndexSitemap: false,
-    generateRobotsTxt: true,
+    additionalPaths: async () => groupPaths.map((path) => ({
+        loc: `/group${path}`,
+        changefreq: 'daily',
+        priority: 0.7,
+    })),
 };
 
 export default config;
